108903d2f077da9ee5aae5d2d5551ae6ead507f0
LuizBido <114614179+LuizBido@users.noreply.github.com>
2022-09-28 18:57:10 +0000

    mudei

--- .ccls-cache/@home@runner@TesteGit/main.c
+++ .ccls-cache/@home@runner@TesteGit/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
-
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+int main(){
+  int cp=0, ci=0, c=0, sp=0, si=0, n;
+  float media;
+  for(c=0; c<=9; c++){
+    printf("digite número inteiro: ");
+    scanf("%d", &n);
+      if(n % 2 == 0){
+        cp++;
+          sp = sp + n;
+    }
+      else if(n % 2 == 1){
+          ci++;
+            si = si + n;
+    }
+  }
+  media = si/ci;
+  printf("número de pares lidos: %d\n", cp);
+  printf("número de ímpares lidos: %d\n", ci);
+  printf("somatório dos pares: %d\n", sp);
+  printf("media dos ímpares: %.2f\n", media);
 }
